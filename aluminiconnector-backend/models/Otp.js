eae803a94897607619e8fc0154dd970a2ece6deb
Mahabub <mahabubx7@gmail.com>
2024-10-01 19:36:34 +0600

    feat: Otp model and schema with ttl

---
+++ aluminiconnector-backend/models/Otp.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const OtpSchema = new mongoose.Schema({
+    otp: { type: String, required: true },
+    action: {
+        type: String,
+        required: true,
+        enum: [
+            'register', 
+            'login', 
+            'reset-password', 
+            'change-email', 
+            'change-password',
+            'verify-email',
+            'verify-phone',
+            // Add more actions here or edit them as needed
+        ]
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        index: true 
+    },
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: false, // optional field
+        index: true,
+    },
+    expiry: {
+        type: Date,
+        required: true,
+        index: { expireAfterSeconds: 0 } // TTL Index
+    }
+});
+
+module.exports = mongoose.model('Otp', OtpSchema);
