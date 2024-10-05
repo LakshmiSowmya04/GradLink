76916bb460ebbd17081ad3168e796650d55d4638
anmol420 <anmolanand2k15@gmail.com>
2024-10-05 17:33:31 +0530

    Added Changes in Validation Middleware

--- backend/lib/schema/userSchema.js
+++ backend/lib/schema/userSchema.js
@@ -1,16 +1,33 @@
 import { z } from "zod";
 
-const userSchema = z.object({
-    username: z.string(),
-    email: z.string().email({ message: "Invalid Email Format."}),
-    password: z.string().min(6, { message: "Password Should Be Minimum Of 6 Characters." }),
-    college: z.string(),
+const signUpSchema = z.object({
+    username: z
+        .string(),
+    email: z
+        .string()
+        .email({ message: "Invalid Email Format."}),
+    password: z
+        .string()
+        .min(6, { message: "Password Should Be Minimum Of 6 Characters." })
+        .max(25, { message: "Password Should Be Maximum Of 25 Characters."}),
+    // college: z
+        // .string(),
     role: z.enum(["student", "alumni"]).refine(
         (role) => role === "student" || role === "alumni",
         { message: "Invalid Role. Role should be either 'student' or 'alumini'." }
     )
 });
 
+const logInSchema = z.object({
+    username: z
+        .string(),
+    password: z
+        .string()
+        .min(6, { message: "Password Should Be Minimum Of 6 Characters." })
+        .max(25, { message: "Password Should Be Maximum Of 25 Characters." }),
+});
+
 export {
-    userSchema
+    signUpSchema,
+    logInSchema
 };
--- backend/middlewares/UserValidator.js
+++ backend/middlewares/UserValidator.js
@@ -1,23 +1,24 @@
 import { z } from "zod";
-import { userSchema } from "../lib/schema/userSchema.js";
 import { errorResponse, successResponse } from "../lib/res/index.js";
 import { StatusCodes } from "http-status-codes";
 
-const validateUser = (req, res, next) => {
-    try {
-        const validateData = userSchema.parse(req.body);
-        req.body = validateData;
-        next();
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            const zError = error.format();
-            return res
-                .status(StatusCodes.FORBIDDEN)
-                .json(new errorResponse(
-                    "Validation Error", { zError }, StatusCodes.FORBIDDEN,
-                ));
+const validateSchema = (schema, source="body") => {
+    function validate(req, res, next) {
+        try {
+            schema.parse(req[source]);
+            next();
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                const zError = error.format();
+                return res
+                    .status(StatusCodes.FORBIDDEN)
+                    .json(new errorResponse(
+                        "Validation Error", { zError }, StatusCodes.FORBIDDEN,
+                    ));
+            }
         }
     }
+    return validate;
 }
 
-export default validateUser;
+export default validateSchema;
