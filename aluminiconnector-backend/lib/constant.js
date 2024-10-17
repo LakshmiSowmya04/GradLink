7e092c48d51981124e0ae55e154c98490396745f
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-10-17 23:00:24 +0530

    deleted backend1

--- aluminiconnector-backend/lib/constant.js
+++
@@ -1,8 +0,0 @@
-const cookieOptions = {
-  httpOnly: true,
-  secure: true,
-  maxAge: 3600000, // Expires in 1 hour
-  sameSite: "Lax",
-};
-
-module.exports = cookieOptions;
--- aluminiconnector-backend/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const PostSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Post", PostSchema);
